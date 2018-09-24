a2af9cbd2a77db50d590441e7c874cedf2ec4dd1
OptimisticReservoir <43356080+OptimisticReservoir@users.noreply.github.com>
2018-09-23 22:49:00 -0700

    Draft with working loops
    The current bug is that main loop only runs once, it doesn't move onto the second word. The recursion might be a bit broken too. Probably for the same reason

--- wordMatcher.py
+++ wordMatcher.py
@@ -3,28 +3,90 @@ def main():
     i=0
     j=0
     dictPath = "/usr/share/dict/words"
-    fullDict=loadDictionary(dictPath)
+    dictFull=loadDictionary(dictPath)
     wordsToMatch=('black','berry','sub','skate','air',
             'pass','water','way','flower','car','horse',
             'up','plane','rail','port','boat','pool','back',
             'side','fish','fall','sun','rain','butter','fly',
             'bow','star','board','walk','road')
-    trimDict=trimDictionary(fullDict,wordsToMatch)
-    tempWords=list(wordsToMatch)
-    for x in tempWords:
+    dictTrim=trimDict(dictFull,wordsToMatch)
+    tempWords=findMatches(dictTrim,list(wordsToMatch))
+    j = len(tempWords)
+    print(str(j)  + " words in list\n" + str(tempWords))
+
+def findMatches(dictionary, wordList, curMatches = []):
+    tempList = wordList
+    newTempL = []
+    allFound = False
+    recursiveMatches = []
+    i = 0
+    j = 0
+    k = ""
+    while not allFound and i < len(tempList):
+        j = i + 1
+        for x in tempList:
+            if j >= len(tempList) - 1: # catch out-of-range error
+                break
+            k = ""
+            tempWord1 = tempList[i] + tempList[j]
+            tempWord2 = tempList[j] + tempList[i]
+            if checkDictionary(dictionary,tempWord1):
+                k = tempWord1
+            if checkDictionary(dictionary, tempWord2):
+                k = tempWord2
+            if k != "":
+                curMatches.append(k)
+                newTempL = tempList
+                newTempL.remove(tempList[j])
+                newTempL.remove(tempList[i])
+                recursiveMatches = findMatches(dictionary,newTempL,curMatches)
+                allFound = [] != recursiveMatches
+                print(curMatches)
+                curMatches.remove(k)
+            j += 1
         i += 1
-    print(str(i) + " words in list\n" + str(tempWords))
+    if len(tempList) == 0:
+        return recursiveMatches
+    else:
+        return []
+
+
 
 def trimDict(dictionary,wordList):
     newDict = []
     for x in dictionary:
         for y in wordList:
             if strContains(x,y):
-                newDict.append(x)
+                newDict.append(x.strip())
                 break
     return newDict
 
-def checkDictionary(word, dictionary):
+def strContains(str1, str2):
+    longStr = str1.lower()
+    shortStr = str2.lower()
+    longLen = len(longStr)
+    shortLen = len(shortStr)
+    if shortLen > longLen:
+        longStr = shortStr
+        shortStr = str1.lower()
+        longLen = shortLen
+        shortLen = len(shortStr)
+    elif len(shortStr) == len(longStr):
+        if shortStr == longStr:
+            return True
+        else:
+            return False
+
+    # At this point, we want to check the start and end of longStr
+    # number of characters to check is shortLen.
+    startStr = longStr[0:shortLen]
+    endStr = longStr[longLen-shortLen : longLen]
+    if startStr == shortStr or endStr == shortStr:
+        return True
+    else:
+        return False
+
+def checkDictionary(dictionary, word):
     match = False
     for x in dictionary:
         if x == word:
